dc0fb29552d5cf07a2f1a7c403b0bfa28c9af447
JohnCiubuc <john.ciubuc@ttuhsc.edu>
2020-09-05 00:27:49 -0500

    Removed temp build

--- Build/CMakeFiles/remarkable-qt_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/remarkable-qt_autogen"
-  "remarkable-qt_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/remarkable-qt_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
